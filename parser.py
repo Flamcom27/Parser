4994127678f45d83082ca3a3a58305df7b1f4757
flamcom27 <27flamcom06@gmail.com>
2022-04-21 13:04:06 +0300

    parser can parse one page

---
+++ parser.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://auto.ria.com/search/?indexName=auto,order_auto,newauto_search&brand.id[0]=84&price.currency=1&abroad.not=0&custom.not=1&country.import.usa.not=-1&page=0&size=10'
+HEADERS = {
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36',
+    'accept': '*/*'
+}
+
+def get_html(url, params=None):
+    r = requests.get(url, headers=HEADERS, params=params) 
+    return r
+
+def get_content(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    items = soup.find_all('section', class_='ticket-item')
+    cars = []
+    for item in items:
+        title = item.find('a', class_='address').get_text().strip()
+        year = title[-4:]
+        cars.append({
+            'title': item.find('a', class_='address').get_text()[0:-5].strip(),
+            'price': item.find('span', class_='bold green size22').get_text().strip(),
+            'year': year
+        })
+    print(cars)
+
+def parse():
+    html = get_html(URL)
+    if html.status_code == 200:
+        get_content(html.text)
+        # print(html.text)
+    else:
+        print('Error')
+
+
+if __name__ == '__main__':
+    parse()
